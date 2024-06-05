534cd01f5f57b900e7e1911e133dd63c401b8037
AlexeyPanchenko <aolpanchenko@gmail.com>
2024-06-05 22:53:12 +0300

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "Shmr24"
+include(":app")
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+dependencyResolutionManagement {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+
